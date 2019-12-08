3abc848ad69fc134239a55bc0afc7c61d556aae0
So1itarius <pavellarionov93@mail.ru>
2019-12-08 23:36:30 +0300

    обозначения для первой задачи по структурам

---
+++ структуры данных/task_1.py
@@ -0,0 +1,48 @@
+"""
+1. Реализовать односвязный список (описание операций в слайдах)
+"""
+
+
+class linked_list(object):
+    pass
+
+    def InsertAtEnd(self):
+        """ InsertAtEnd – вставка в конец. """
+
+    pass
+
+    def InsertAtHead(self):
+        """  InsertAtHead– вставка в начало. """
+
+    pass
+
+    def Delete(self):
+        """ Delete– удаление указанного элемента."""
+
+    pass
+
+    def DeleteAtHead(self):
+        """DeleteAtHead – удаление первого элемента. """
+
+    pass
+
+    def Search(self):
+        """ Search– получение указанного элемента."""
+
+    pass
+
+    def isEmpty(self):
+        """– возвращает true, если связный список пуст."""
+
+    pass
+
+
+class node(object):
+
+    def __init__(self, data, pointer):
+        self.data = data
+        self.pointer = pointer
+
+
+if __name__ == "__main__":
+    pass
