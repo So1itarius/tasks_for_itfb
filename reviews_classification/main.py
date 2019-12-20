58acd6f63fb8dd7f18d6563b7f172bc8da3b983a
So1itarius <pavellarionov93@mail.ru>
2019-12-20 12:36:36 +0300

    задача 5 (ver 1)

--- reviews_classification/main.py
+++ reviews_classification/main.py
@@ -1,15 +1,38 @@
+import re
+
 from nltk.stem import SnowballStemmer
 
+from reviews_classification.patterns import POSITIVE, NEGATIVE
+
 snowball_stemmer = SnowballStemmer("russian")
-i = 0
+
+
+def converter(arr):
+    i = 0
+    for word in arr:
+        stem = snowball_stemmer.stem(word)
+        arr[i] = stem
+        i += 1
+    return arr
+
+
 with open('texts_opinions.txt', 'r', encoding='utf-8') as f:
-    for line in f:
-        line = line.strip().split(' ')
-        print(line)
-        for word in line:
-            #word = line[0]
-            #wordCount = line[1]
-            stem = snowball_stemmer.stem(word)
-            print(word, ''.join(stem))
-
-
+    with open('texts_ratings.txt', 'r', encoding='utf-8') as f1:
+        con_pos = converter(POSITIVE)
+        con_neg = converter(NEGATIVE)
+        for line, line1 in zip(f, f1):
+            rep = re.sub(r'[!\"#$%&\'(\-)*+,›./:;<=>?@[\]^_\\`{|}~]', '', line)
+            line = rep.strip().lower().split(' ')
+            p = 0
+            n = 0
+            for word in converter(line):
+                if word in con_pos:
+                    p += 1
+                elif word in con_neg:
+                    n += 1
+            if p > n:
+                print(f"Результат обработки: positive, «правильная» классификаци: {line1}")
+            elif p < n:
+                print(f"Результат обработки: negative, «правильная» классификаци: {line1}")
+            else:
+                print(f"Результат обработки: undef, «правильная» классификаци: {line1}")
